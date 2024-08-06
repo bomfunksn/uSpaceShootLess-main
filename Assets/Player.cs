bf26912138291aaeac85fcbe631eeaa1c9384fa7
bomfunksn <xpehbomfunk@gmail.com>
2024-08-06 19:31:29 +0200

    movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,8 +5,18 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    Vector2 rawInput;
+
     void Update()
     {
-        
+        Vector3 delta = rawInput;
+        transform.position += delta;
     }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+        Debug.Log(rawInput); 
+    }
+
 }
